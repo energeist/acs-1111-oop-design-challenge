f24513ecade8cff6d525473043a18ae2e72e1eab
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-11-28 19:07:42 -0800

    Update players.py

--- players.py
+++ players.py
@@ -1,2 +1,26 @@
+from cards import Cards
+from cards import Deck
+import random
+
 class Person:
-    def __init__(self, value, suit, deck):
+    def __init__(self,name):
+        self.name = name
+        self.hand = []
+
+    def greet_person(self):
+        print(f"Hi, {self.name} ") # will add to it 
+
+    def draw_card(self, deck):
+        brand_new_card = deck.deal()
+        self.hand.append(brand_new_card)
+        brand_new_card.show_cards()
+
+    def sort(self, cards):
+        self.cards = cards
+
+    def remove_card(self):
+        pass
+
+    def show_cards(self):
+        for card in self.cards:
+            print(card)
